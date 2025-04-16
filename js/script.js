a4ed7b45b9b54ac6309a4735562175920d39cc5c
Annisafitrianirizky <Whatshine01@gmail.com>
2025-04-16 17:10:30 +0700

    Banner slideshow update

--- js/script.js
+++ js/script.js
@@ -26,3 +26,28 @@ function setName(nama) {
     document.getElementById("name").innerHTML = nama
     document.getElementById("error-name").innerHTML = ""
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("img-slideshow");
+  if (n > x.length) slideIndex = 1;
+  else if (n < 1) slideIndex = x.length;
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+
+  console.log("Current slide index: " + slideIndex); // Debugging line
+}
+
+setInterval(function() {
+  plusDivs(1);
+}, 1000); // Change image every 3 seconds
